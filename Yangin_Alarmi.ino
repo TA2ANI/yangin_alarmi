12ac70a1a69517c0e5c7490763ff060d0224fa13
TA2ANI <44003703+TA2ANI@users.noreply.github.com>
2018-12-17 00:39:34 +0300

    Add files via upload

---
+++ Yangin_Alarmi.ino
@@ -0,0 +1,59 @@
+
+#define BLYNK_PRINT Serial
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+BlynkTimer timer;
+
+char auth[] = "TOKEN";
+
+char ssid[] = "Wifi_ Adı";
+char pass[] = "WiFi_Şifresi";
+
+int alev = A0;
+int alevdurum ;
+int tur;
+int buzzer = 5;     // Bağlı olduğu pinin GPIO karşılığını girin
+
+WidgetLED led1 (V0);
+
+void setup()
+{
+  pinMode (alev, INPUT);
+  pinMode (buzzer, OUTPUT);
+  Serial.begin(9600);
+  Blynk.begin(auth, ssid, pass);
+  timer.setInterval (1000L, dongu);
+}
+
+
+void dongu ()
+{
+  alevdurum = analogRead (alev);
+
+  if (alevdurum < 800)
+  {
+    tone (buzzer, 650);
+    led1.on();
+    tur = 1;
+
+    if (tur == 1)
+    {
+      Blynk.notify ("DIKKAT !!! ALEV ALGILANDI");
+      tur = 0;
+    }
+  }
+  else
+  {
+    noTone (buzzer);
+    led1.off();
+    tur = 0;
+  }
+}
+
+
+void loop()
+{
+  Blynk.run();
+  timer.run ();
+}
+
